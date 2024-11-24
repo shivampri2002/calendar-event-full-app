c6c73d9d2197b50e6cb70f2a4f66f2560d8ed17b
shivam <shivprishi@gmail.com>
2024-11-24 23:27:36 +0530

    Build error rectified2

--- calendar_event_app_frontend/calendar-event-app/src/services/firebase.ts
+++ calendar_event_app_frontend/calendar-event-app/src/services/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
