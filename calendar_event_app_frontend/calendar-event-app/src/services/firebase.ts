bfc6c7e6300b5512a7b1261cd41646dcaea38352
shivam <shivprishi@gmail.com>
2024-11-24 23:26:03 +0530

    Build error rectified1

--- calendar_event_app_frontend/calendar-event-app/src/services/firebase.ts
+++ calendar_event_app_frontend/calendar-event-app/src/services/firebase.ts
@@ -12,6 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
